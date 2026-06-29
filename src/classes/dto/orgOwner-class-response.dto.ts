61653d34d1d360e1e0844323d45ab61e4f35a7eb
ziad078 <ziadzayd79@gmail.com>
2026-06-29 09:06:13 +0300

    cto report

--- src/classes/dto/orgOwner-class-response.dto.ts
+++ src/classes/dto/orgOwner-class-response.dto.ts
@@ -1,8 +1,7 @@
-import { GradeName } from 'src/common/enums/grade-name.enum';
 import { BaseClassResponse } from './base-class-response.dto';
 import { OrganizationChild } from 'src/children/entities/organization-child.entity';
 
 export interface OrgOwnerClassResponse extends BaseClassResponse {
-  gradeName: GradeName;
+  gradeName: string;
   children: Partial<OrganizationChild>[];
 }
